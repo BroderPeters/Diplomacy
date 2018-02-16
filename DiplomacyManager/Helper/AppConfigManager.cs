b8ae0734ab07ffbcd0c658ae0cbca5944be866c2
unknown <contact@broderpeters.de>
2018-02-17 00:46:31 +0100

    Added manager for application configuration

---
+++ DiplomacyManager/Helper/AppConfigManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiplomacyManager.Helper
+{
+    public class AppConfigManager
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+        public static void Configure(bool withDebug)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true);
+            Configuration = builder.Build();
+        }
+    }
+}
